998a154c6c545afe2ca683d1163f7873aa8eb1da
oesel <oesel@yandex.ru>
2022-12-04 23:33:06 +0300

    added solution to Task6

---
+++ Seminar1_Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Считываем данные с консоли
+string? imputNumLine = Console.ReadLine();
+// Проверим что данные не пустые
+if(imputNumLine!=null)
+{
+// Парсим число, используем оператор %
+    int imputNumber = int.Parse(imputNumLine);
+    int imputNumberB = imputNumber % 2;
+// Проверяем условие, выводим в консоль
+    if(imputNumberB == 0)
+    {
+        Console.WriteLine("четное число");
+    }
+    else
+    {
+        Console.WriteLine("нечетное число");
+    }
+}
